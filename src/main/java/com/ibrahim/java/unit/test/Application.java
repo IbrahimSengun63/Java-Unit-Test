c6ebe5d9a2cfe46f14493225135e6297ac947cec
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-07-25 10:34:44 +0300

    Initialize unit test exercise project.

---
+++ src/main/java/com/ibrahim/java/unit/test/Application.java
@@ -0,0 +1,13 @@
+package com.ibrahim.java.unit.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
